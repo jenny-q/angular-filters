3384f70acd059d19d56e5c2320f66fdf0af7151c
JennyQ <jennifercquispe@gmail.com>
2021-10-18 11:53:25 -0400

    adding bootstrap 5, filter bar component, bem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FilterBarComponent } from './filter-bar/filter-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterBarComponent
   ],
   imports: [
     BrowserModule
